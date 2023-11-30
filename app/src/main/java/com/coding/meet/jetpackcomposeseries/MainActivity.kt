e622d4d251a301e9b889d6f1aa56dce367e6602b
coding meet <codingmeet26@gmail.com>
2023-11-30 18:46:13 +0530

    implement textfield

---
+++ app/src/main/java/com/coding/meet/jetpackcomposeseries/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.coding.meet.jetpackcomposeseries
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.unit.*
+import androidx.compose.ui.tooling.preview.Preview
+import com.coding.meet.jetpackcomposeseries.ui.theme.JetpackComposeSeriesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeSeriesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TextFieldScreen()
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetpackComposeSeriesTheme {
+        TextFieldScreen()
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TextFieldScreen() {
+    var firstNameState by remember { mutableStateOf("") }
+    var lastNameState by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        TextField(
+            value = firstNameState,
+            onValueChange = { firstNameState = it },
+            label = { Text("First Name") }
+        )
+        OutlinedTextField(
+            value = lastNameState,
+            onValueChange = { lastNameState = it },
+            label = { Text("Last Name") }
+        )
+
+        Text(text ="$firstNameState $lastNameState")
+    }
+}
