f12cc3413dac015d37978ffbc69552143294823b
coding meet <codingmeet26@gmail.com>
2023-12-04 18:22:34 +0530

    implement image

--- app/src/main/java/com/coding/meet/jetpackcomposeseries/MainActivity.kt
+++ app/src/main/java/com/coding/meet/jetpackcomposeseries/MainActivity.kt
@@ -16,9 +16,11 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.*
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.*
 import androidx.compose.ui.text.*
 import androidx.compose.ui.text.font.*
 import androidx.compose.ui.text.style.*
@@ -35,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    BoxScreen()
+                    ImageScreen()
                 }
             }
         }
@@ -284,5 +286,35 @@ fun BoxScreen() {
             Text(text = "Hello World", modifier = Modifier.align(Alignment.BottomEnd))
         }
 
+    }
+}
+
+@Composable
+fun ImageScreen() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "hello",
+            modifier = Modifier
+                .size(200.dp)
+                .clip(RoundedCornerShape(10))
+                .background(Color.Black)
+                .border(5.dp, Color.Gray, RoundedCornerShape(10)),
+            contentScale = ContentScale.Fit,
+            alignment = Alignment.Center
+        )
+
+        Icon(
+            painter = painterResource(id = R.drawable.ic_add_shopping_cart_24),
+            tint = Color.Red,
+            contentDescription = "",
+            modifier = Modifier.size(100.dp)
+        )
+        IconButton(onClick = { /*TODO*/ }, modifier = Modifier.size(100.dp)) {
+            Icon(imageVector = Icons.Filled.Home, contentDescription = "", modifier = Modifier.fillMaxSize())
+        }
     }
 }
