039966019f51923eef434c69f2ab74b9058c5041
coding meet <codingmeet26@gmail.com>
2023-12-22 11:07:32 +0530

    custom saver

--- app/src/main/java/com/coding/meet/jetpackcomposeseries/components/StateScreen.kt
+++ app/src/main/java/com/coding/meet/jetpackcomposeseries/components/StateScreen.kt
@@ -1,5 +1,7 @@
 package com.coding.meet.jetpackcomposeseries.components
 
+import android.os.Parcel
+import android.os.Parcelable
 import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,7 +9,9 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
 import com.coding.meet.jetpackcomposeseries.MainViewModel
+import java.io.Serializable
 
 /**
  * @author Coding Meet
@@ -70,4 +74,69 @@ fun MutableStateListExample() {
         }
     }
 
+}
+
+
+data class Student(var name:String, var age: Int)
+
+
+val studentValSaver = Saver<Student,Map<String,Any>>(
+    save = {
+        mapOf(
+            "name" to it.name,
+            "age" to it.age
+        )
+    },
+    restore = {
+        val name = it["name"] as String
+        val age = it["age"] as Int
+        Student(name,age)
+    }
+)
+
+class StudentSaverClass() : Saver<Student?,Map<String,Any>>{
+    override fun restore(value: Map<String, Any>): Student {
+        val name = value["name"] as String
+        val age = value["age"] as Int
+       return Student(name,age)
+    }
+
+    override fun SaverScope.save(value: Student?): Map<String, Any>? {
+      value?.let {
+          return  mapOf(
+              "name" to value.name,
+              "age" to value.age
+          )
+      } ?: return null
+
+    }
+
+}
+
+@Composable
+fun CustomSaverExample() {
+
+    var studentModel by rememberSaveable(
+        stateSaver = StudentSaverClass()
+    ) {
+        mutableStateOf<Student?>(null)
+    }
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        studentModel?.let { it
+            Text(text = "name: ${it.name}", fontSize = 30.sp)
+            Text(text = "age: ${it.age}", fontSize = 30.sp)
+        }
+        Button(onClick = {
+            studentModel = Student(
+                name = "meet",
+                age = 21
+            )
+
+        }) {
+            Text(text = "Update field")
+        }
+    }
 }
