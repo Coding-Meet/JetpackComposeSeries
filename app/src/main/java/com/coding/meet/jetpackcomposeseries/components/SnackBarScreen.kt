e8dcb113209dff2c1e95385d0ec5d99e799d87a8
coding meet <codingmeet26@gmail.com>
2024-01-11 10:21:35 +0530

    snackbar in jetpack compose

---
+++ app/src/main/java/com/coding/meet/jetpackcomposeseries/components/SnackBarScreen.kt
@@ -0,0 +1,95 @@
+package com.coding.meet.jetpackcomposeseries.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import kotlinx.coroutines.launch
+
+/**
+ * @author Coding Meet
+ * Created 10-01-2024 at 01:45 pm
+ */
+
+@Composable
+fun BasicSnackBarScreen() {
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        },
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                text = { Text("Show snackbar") },
+                icon = { Icon(Icons.Filled.Settings, contentDescription = "") },
+                onClick = {
+                    scope.launch {
+                        val result = snackbarHostState.showSnackbar("Snackbar", duration = SnackbarDuration.Long)
+
+                        when (result) {
+                            SnackbarResult.ActionPerformed -> {
+                                /* Handle snackbar action performed */
+                            }
+                            SnackbarResult.Dismissed -> {
+                                /* Handle snackbar dismissed */
+                            }
+                        }
+                    }
+                }
+            )
+        }
+    ) { contentPadding ->
+        // Screen content
+        Text(text = "Coding Meet",Modifier.padding(contentPadding))
+    }
+}
+
+@Composable
+fun SnackBarWithActionScreen() {
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        },
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                text = { Text("Show snackbar") },
+                icon = { Icon(Icons.Filled.Settings, contentDescription = "") },
+                onClick = {
+                    scope.launch {
+                        val result = snackbarHostState
+                            .showSnackbar(
+                                message = "Snackbar",
+                                actionLabel = "Action",
+                                // Defaults to SnackbarDuration.Short
+                                duration = SnackbarDuration.Indefinite
+                            )
+                        when (result) {
+                            SnackbarResult.ActionPerformed -> {
+                                /* Handle snackbar action performed */
+                            }
+                            SnackbarResult.Dismissed -> {
+                                /* Handle snackbar dismissed */
+                            }
+                        }
+                    }
+                }
+            )
+        }
+    ) { contentPadding ->
+        // Screen content
+    }
+}
